14bd9f167319c9cda87f3ec9ca0169d31c570af9
Carlos Fisac Ferrández <charles8ff@gmail.com>
2022-04-15 20:41:14 +0200

    cleaned import and improved comments

--- endless.py
+++ endless.py
@@ -1,8 +1,8 @@
-import pyautogui
+import pyautogui as pya
 import time
 import keyboard
 
-#made this measures in 1980*1080, feel free to change for other resolutions
+# made this measures in 1980*1080, feel free to change for other resolutions
 column = 1750 
 upgrades = 150 
 cursors = 310 
@@ -15,7 +15,8 @@ reincarnateX = 945
 reincarnateY = 110
 
 controller = True # manual stop if needed, pressing q
-loops= 1
+loops = 1
+targetAscensions = 1000 # change this number if you don't need to do 1000 ascensions
 endingDelay = loops * 0.05 # adds a delay before ascending
 
 tinySleep = 0.2
@@ -23,77 +24,76 @@ mediumSleep = 0.5
 
 
 def buy100s():
-    pyautogui.keyDown('shift')
-    pyautogui.click()
+    pya.keyDown('shift')
+    pya.click()
     time.sleep(tinySleep)
-    pyautogui.click()
+    pya.click()
     time.sleep(tinySleep)
-    pyautogui.click()
-    pyautogui.keyUp('shift')
+    pya.click()
+    pya.keyUp('shift')
 
 def buyupgrades():
-    pyautogui.press('home')
+    pya.press('home') # this key scrolls up
     time.sleep(mediumSleep)
-    pyautogui.moveTo(column, upgrades, duration = 0)
-    pyautogui.click()
+    pya.moveTo(column, upgrades, duration = 0)
+    pya.click()
     time.sleep(mediumSleep)
-    pyautogui.click()
+    pya.click()
     time.sleep(mediumSleep)
-    pyautogui.click()
-    pyautogui.moveTo(column, upgrades, duration = 0)
+    pya.click()
+    pya.moveTo(column, upgrades, duration = 0)
     
 def buyBuildings():
     time.sleep(mediumSleep)
     buyupgrades() # scrolls auto up again  
     time.sleep(mediumSleep) 
-    pyautogui.moveTo(column, cursors, duration = 0)
+    pya.moveTo(column, cursors, duration = 0)
     buy100s()
-    pyautogui.moveTo(column, grandmas, duration = 0)
+    pya.moveTo(column, grandmas, duration = 0)
     buy100s()
     time.sleep(mediumSleep) 
     buyupgrades()
-    pyautogui.moveTo(column, grandmas, duration = 0)
-    pyautogui.click()
-    pyautogui.press('end')  # scroll down again
+    pya.moveTo(column, grandmas, duration = 0)
+    pya.click()
+    pya.press('end')  # scroll down again
     time.sleep(1)
-    pyautogui.moveTo(column, fractals, duration = 0)
+    pya.moveTo(column, fractals, duration = 0)
     buy100s()
-    pyautogui.moveTo(column, idleverses, duration = 0)
+    pya.moveTo(column, idleverses, duration = 0)
     buy100s()
     
-#adding a manual stop, this will finish the ascension and stop from looping
+# adding a manual stop, this will finish the ascension and stop from looping
 def stop():
     if keyboard.is_pressed('q'):
         print('Handmade Stop')
         global controller
         controller = False
-        
+
+# adding a wrapper function 
 def oneRound():
     buyBuildings()
     stop() # press q
     
 time.sleep(3)      
-#starts in 3, tab into cookieclicker
-while controller and loops<=1000:
-    #add more oneRounds if you don't achieve +1 ascensions with 2 of them
+# starts in 3 seconds, tab into CookieClicker
+while controller and loops <= targetAscensions:
+    # add more oneRounds if you don't achieve +1 prestige with 2 of them
     oneRound()
     oneRound() 
     time.sleep(mediumSleep)
     buyupgrades()
     stop() # press q
-    time.sleep(1+endingDelay)
-    # maybe this stop is not needed depending on your prestige
+    time.sleep(1+endingDelay) # maybe this delay is not needed depending on your prestige level
     # reincarnation
-    pyautogui.press('home')
+    pya.press('home')
     time.sleep(0.2)
-    pyautogui.moveTo(legacyX, legacyY, duration = 0)
-    pyautogui.click()
+    pya.moveTo(legacyX, legacyY, duration = 0)
+    pya.click()
     time.sleep(0.5)
-    pyautogui.press('enter')
+    pya.press('enter')
     time.sleep(0.5)
-    pyautogui.press('esc')
-    pyautogui.moveTo(reincarnateX, reincarnateY, duration = 0)
-    pyautogui.click()
-    pyautogui.press('enter')
-    #loop counter
-    loops+=1
+    pya.press('esc')
+    pya.moveTo(reincarnateX, reincarnateY, duration = 0)
+    pya.click()
+    pya.press('enter')
+    loops+=1 # loop counter
